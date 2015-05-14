8a7c6444da1fe455d1e92efd265f9f9e1787f4ad
saiPA <saipraneeth_a@hotmail.com>
2015-05-14 22:23:18 +1200

    Initial commit

---
+++ member.php
@@ -0,0 +1,23 @@
+<?php 
+session_start();
+if(!isset($_SESSION["sess_user"])){
+	header("location:login.php");
+} else {
+?>
+<!doctype html>
+<html>
+<head>
+<title>Welcome</title>
+</head>
+<body>
+<h2>Welcome, <?=$_SESSION['sess_user'];?>! <a href="logout.php">Logout</a></h2>
+<p>
+Welcome to the Wiki.
+</p>
+</body>
+</html>
+<?php
+}
+?>
+
+
